147704816dd383dc48f169d847ceda8370dde798
Diana <tsarkova.92@mail.ru>
2020-04-25 13:52:03 +0300

    Initial Commit

---
+++ testTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by thisdjango on 25.04.2020.
+//  Copyright © 2020 thisdjango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
